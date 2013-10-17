cab06572a2d36528461acd28e79fb2286a8fb5f0
Stephen Sugden <me@stephensugden.com>
2013-10-16 17:05:10 -0700

    Forward child process errors all the way out

--- index.js
+++ index.js
@@ -433,6 +433,7 @@ Bash.prototype.eval = function (line) {
         if (!redirected) cmd.pipe(output, { end: false });
         
         cmd.on('exit', function (code) { exitCode = exitCode || code });
+        cmd.on('error', function (err) { output.emit('error', err) });
         cmd.on('end', function () {
             for (var next = commands[0]; next && next.op; next = commands[0]) {
                 commands.shift();
@@ -499,6 +500,7 @@ Bash.prototype.eval = function (line) {
             var stdin = p.stdin || resumer();
             var d = duplexer(stdin, p.stdout);
             p.on('exit', function (code) { d.emit('exit', code) });
+            p.on('error', function (e) { d.emit('error', e) });
             return d;
         }
         if (!p) {
