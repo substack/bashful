37c74d8a8ef0117f863d091cd9d0788472ec510f
Christian Tegnér <christian.tegner@gmail.com>
2013-11-24 13:26:50 +0100

    Handle +/-N arguments outside range

--- test/pushd.js
+++ test/pushd.js
@@ -119,3 +119,81 @@ test('pushd with no arguments handles missing destination dir', function (t) {
     }));
     s.end('pushd; echo $?; pwd;');
 });
+
+test('pushd with stack rotation argument on empty stack', function (t) {
+    t.plan(3);
+
+    var sh = bash({
+        spawn: function (cmd) { t.fail('spawn ' + cmd) },
+        env: {
+            PS1: '$ ',
+            PWD: '/beep/boop',
+            HOME: '/home/robot'
+        },
+        exists: function (cmd) { t.fail('exists ' + cmd) }
+    });
+
+    var s = sh.createStream();
+    s.pipe(concat(function (src) {
+        t.equal(src + '', '$ pushd: +1: directory stack index out of range\n1\n/beep/boop\n');
+        t.same(sh.dirs, []);
+        t.equal(sh.env.PWD, '/beep/boop');
+    }));
+    s.end('pushd +1; echo $?; pwd;');
+});
+
+test('pushd with stack rotation argument greater than stack size', function (t) {
+    t.plan(3);
+
+    var sh = bash({
+        spawn: function (cmd) { t.fail('spawn ' + cmd) },
+        env: {
+            PS1: '$ ',
+            PWD: '/beep/boop',
+            HOME: '/home/robot'
+        },
+        exists: function (cmd) { t.fail('exists ' + cmd) }
+    });
+    sh.dirs = [
+        '/beep/boop',
+        '/home/robot'
+    ];
+
+    var s = sh.createStream();
+    s.pipe(concat(function (src) {
+        t.equal(src + '', '$ pushd: -9: directory stack index out of range\n1\n/beep/boop\n');
+        t.same(sh.dirs, [
+            '/beep/boop',
+            '/home/robot'
+        ]);
+        t.equal(sh.env.PWD, '/beep/boop');
+    }));
+    s.end('pushd -9; echo $?; pwd;');
+});
+
+test('pushd avoids false positives for stack rotation arguments', function (t) {
+    t.plan(4);
+
+    var sh = bash({
+        spawn: function (cmd) { t.fail('spawn ' + cmd) },
+        env: {
+            PS1: '$ ',
+            PWD: '/beep/boop',
+            HOME: '/home/robot'
+        },
+        exists: function (file, cb) {
+            t.equal(file, '/42-things-you-should-try-while-in-oakland');
+            cb(file === '/42-things-you-should-try-while-in-oakland');
+        }
+    });
+
+    var s = sh.createStream();
+    s.pipe(concat(function (src) {
+        t.equal(src + '', '$ /42-things-you-should-try-while-in-oakland /beep/boop\n0\n/42-things-you-should-try-while-in-oakland\n');
+        t.same(sh.dirs, [
+            '/beep/boop'
+        ]);
+        t.equal(sh.env.PWD, '/42-things-you-should-try-while-in-oakland');
+    }));
+    s.end('pushd /42-things-you-should-try-while-in-oakland; echo $?; pwd;');
+});
